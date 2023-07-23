7a512b2471008f6c96e45fef6bac8b62fdb26d37
Umma Ahimsha <uahimsha@gmail.com>
2023-07-23 16:35:54 +0700

    fix seo

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,8 +3,8 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   url: 'https://hydego.id',
   name: 'Umma Ahimsha',
-  description: `I'm a freelance web developer from Indonesia, specialized in providing
-                web solutions using popular technologies. Freelance services for your seamless digital experiences.`,
+  description:
+    "I'm a freelance web developer from Indonesia, specialized in providing web solutions using popular technologies. Freelance services for your seamless digitalexperiences.",
   mainNav: [
     {
       title: 'Home',
